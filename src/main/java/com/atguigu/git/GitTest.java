bcbc0f6234301fce3c927c7e402b13a037aaa675
uzumaki0naruto <73424437+uzumaki0naruto@users.noreply.github.com>
2021-09-18 18:13:04 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,7 +7,8 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello hot fix");
-        System.out.println("this  master ");
+        System.out.println("this  master");
         System.out.println("hello hot test");
+        System.out.println("alerday login git hub");
     }
 }
